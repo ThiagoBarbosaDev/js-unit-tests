940a27d916df4c6ddd776461dd2a47ab609bbbe1
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-11 17:43:01 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,14 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
+let feedbackHandler = () => 'Eita pessoa boa!';
 
-const createStudent = () => {};
+const createStudent = (aluno) => {
+  let newAluno = {
+    name: aluno,
+    feedback: feedbackHandler,
+  };
+  return newAluno;
+};
 
 module.exports = createStudent;
